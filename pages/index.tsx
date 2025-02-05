5d2dc7528d758eed15334c5ed1c5ac6fe241c3e5
shreerang6921 <68148922+shreerang6921@users.noreply.github.com>
2025-02-05 15:14:33 +0530

    Update index.tsx CUSTOM 33333

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://www.laconic.com/">Laconic! TESTING AUCTION WITH CUSTOM DOMMMAAAAA 2222</a>
+          Welcome to <a href="https://www.laconic.com/">Laconic! TESTING AUCTION WITH CUSTOM DOMAIN 3333333333333333333</a>
         </h1>
 
         <div className={styles.grid}>
