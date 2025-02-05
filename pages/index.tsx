38c20cfeada873a7133f311e033946c1eac2cdef
shreerang6921 <68148922+shreerang6921@users.noreply.github.com>
2025-02-05 15:07:41 +0530

    Update index.tsx DOMMMAAAAA

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://www.laconic.com/">Laconic! TESTING AUCTION WITH CUSTOM DOMAIN</a>
+          Welcome to <a href="https://www.laconic.com/">Laconic! TESTING AUCTION WITH CUSTOM DOMMMAAAAA</a>
         </h1>
 
         <div className={styles.grid}>
