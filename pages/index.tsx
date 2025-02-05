82131060530ab559a514fe580166fc6cfe7a8f21
shreerang6921 <68148922+shreerang6921@users.noreply.github.com>
2025-02-05 14:57:18 +0530

    Update index.tsx auctions with custom D

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://www.laconic.com/">Laconic! NEW COMMIT TRIGGER</a>
+          Welcome to <a href="https://www.laconic.com/">Laconic! TESTING AUCTION WITH CUSTOM D</a>
         </h1>
 
         <div className={styles.grid}>
